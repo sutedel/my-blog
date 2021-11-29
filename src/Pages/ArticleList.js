7a6b4ddcc6c3dbca7fc09c8ee960469123409b8a
Ahmad Jafari <71406620+sutedel@users.noreply.github.com>
2021-11-29 00:57:03 -0500

    Create Article List, and link to content

--- src/Pages/ArticleList.js
+++ src/Pages/ArticleList.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import articleContent from './article-content';
+
+
 const ArticleList=()=>(
-<>
-<h1>Article list!</h1>
-</>
+           <>
+                <h1>Article list!</h1>
+                {
+                    articleContent.map((article,key)=>(
+                   <Link key={key} to={`/article/${article.name}`}>
+                   <h3>{article.title}</h3>
+                   </Link>))
+                }
+           </>
 );
 export default ArticleList;
